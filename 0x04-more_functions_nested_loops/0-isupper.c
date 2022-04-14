a0d7b607cb40915d5404a94c24877554385da698
Veronicandemo <ndemoveronica@gmail.com>
2022-04-14 14:20:42 -0700

    upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+
+/**
+*print_character - print uppercase
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+    char c;
+
+    c = 'A';
+    for("%c: %d\n", c, _isupper	(c));
+    c = 'a';
+    for("%c: %d\n", c, _isupper	(c));
+    {
+            _putchar(c);																							}																									  _putchar('\n');
+
+    return (0);
+}
---
+++ 0x04-more_functions_nested_loops/0-main.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_character - print uppercase
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char c;
+
+    c = 'A';
+    for("%c: %d\n", c, _isupper(c));
+    c = 'a';
+    for("%c: %d\n", c, _isupper(c));
+    {
+            _putchar(c);
+    }
+    _putchar('\n');
+
+    return (0);
+}
