534c38c23f6b43faa85f98832e923a1a0bab209e
Veronicandemo <ndemoveronica@gmail.com>
2022-04-17 11:10:30 -0700

    Isupper

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,5 @@
-#ifndef 
-#define
+#ifndef MAIN_H
+#define MAIN_H
 
 int _isupper(int c);
 int _isdigit(int c);
@@ -7,10 +7,10 @@ int mul(int a, int b);
 int _putchar(char c);
 void print_numbers(void);
 void print_most_numbers(void);
-void :more_numbers(void);
+void:more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
-endif /* MAIN_H */
+#endif /* MAIN_H */
