3a3d4c467aaa10119c5f220415c774e63245f2fd
Veronica Ndemo <ndemoveronica@gmail.com>
2022-05-17 01:04:39 +0300

    Prints a name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,52 +1,14 @@
-#include <stdio.h>
+#include <stdlib.h>
 #include "function_pointers.h"
 
 /**
- * print_name_as_is - prints a name as is
- * @name: name of the person
- *
- * Return: Nothing.
+ * print_name - prints a name
+ * @name: name to print
+ * @v: pointer to the printing function
  */
-void print_name_as_is(char *name)
+void print_name(char *name, void (*v)(char *))
 {
-    printf("Hello, my name is %s\n", name);
-}
-
-/**
- * print_name_uppercase - print a name in uppercase
- * @name: name of the person
- *
- * Return: Nothing.
- */
-void print_name_uppercase(char *name)
-{
-    unsigned int i;
-
-    printf("Hello, my uppercase name is ");
-    i = 0;
-    while (name[i])
-    {
-        if (name[i] >= 'a' && name[i] <= 'z')
-        {
-            putchar(name[i] + 'A' - 'a');
-        }
-        else
-        {
-            putchar(name[i]);
-        }
-        i++;
-    }
-}
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    print_name("Veronica", print_name_as_is);
-    print_name("Veronica Ndemo", print_name_uppercase);
-    printf("\n");
-    return (0);
+	if (!name || !v)
+		return;
+	v(name);
 }
